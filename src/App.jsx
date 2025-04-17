216e035cbc993afada5649eca0c44a8ca9da0f8e
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-04-17 23:54:38 +0530

    setup Redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Login from "./Login";
-import Profile from "./Profile";
-import Body from "./Body";
-import NavBar from "./NavBar"; 
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
+import  {Provider} from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
 
 function App() {
   
 
   return (
     <div data-theme="dark" className="min-h-screen">
-      
-      <BrowserRouter basename="/">
-        <Routes>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
             <Route path="/" element={<Body/>}>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      
+              <Route path="/" element={<Feed/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
       
     
       
