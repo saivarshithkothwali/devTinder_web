687bd7a70804cf5fb9a98daca960c3bfa64d6a0d
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-04-21 17:53:27 +0530

    built logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import {BASE_URL} from "../utils/constants";
 const Login = () => {
   const [emailId,setEmailId]=useState("tony@gmail.com");
   const [password,setPassword]=useState("Tony@123");
+  const [error,setError]=useState("");
   const dispatch=useDispatch();
   const navigate=useNavigate();
 
@@ -24,7 +25,8 @@ const Login = () => {
   return navigate("/");
     }
     catch(err){
-      console.error(err);
+      setError(err?.response?.data || "Something went wrong");
+      
     }
   }
   return (
@@ -43,6 +45,7 @@ const Login = () => {
               <input type="text" value={password} className="input" onChange={(e)=>setPassword(e.target.value)}/>
             </fieldset>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>Login</button>
           </div>
