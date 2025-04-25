c60902cf13b2b9d43f4830b9fc7ead924eb5bfed
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-04-25 19:08:47 +0530

    Creating connection page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -45,7 +45,9 @@ const NavBar = () => {
             <span className="badge">New</span>
           </Link>
         </li>
-        <li><a>Settings</a></li>
+        <li>
+          <Link to="/Connections">Connections</Link>
+        </li>
         <li><a onClick={handleLogout}>Logout</a></li>
       </ul>
     </div>
