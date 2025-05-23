09f1acec64aeb31f4ea90bbde4a9544acf7a9278
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-05-23 11:12:31 +0530

    Updating livechat feature

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,12 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection=()=>{
-  return io(BASE_URL);
+  
+  if(location.hostname==="localhost")
+  {
+    return io(BASE_URL);
+  }
+  else{
+    return io("/", {path: "/api/socket.io"});
+  }
 };
