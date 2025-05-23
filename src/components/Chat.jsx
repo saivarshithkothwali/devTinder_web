4461b053c5c42b5009ce3f66ada6adf2a86abd51
Sai Varshith Kothwali <saivarshithkothwali@gamil.com>
2025-05-23 11:12:31 +0530

    Updating livechat feature

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,25 +2,47 @@ import {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const Chat = () => {
   const {targetUserId}=useParams();
-  
   const [messages,setMessages]=useState([]);
   const [newMessage,setNewMessage]=useState("");
   const user=useSelector((store)=>store.user);
   const userId=user?._id;
+
+  const fetchChatMessages=async()=>{
+    const chat=await axios.get(BASE_URL+ "/chat/" +targetUserId,{
+      withCredentials:true,
+    });
+    console.log(chat.data.messages);
+
+    const chatMessages=chat?.data?.messages.map(msg=>{
+      const {senderId,text}=msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text,
+      };
+    });
+    setMessages(chatMessages);
+  };
+
+  useEffect(()=>{
+    fetchChatMessages();
+  },[]);
 
   useEffect(()=>{
     if(!userId){
       return;
     }
     const socket=createSocketConnection();
-    socket.emit("joinChat",{firstName:user.firstName,userId,targetUserId});
+    socket.emit("joinChat",{firstName:user.firstName,lastName:user.lastName,userId,targetUserId});
 
-    socket.on("messageReceived",({firstName,text})=>{
+    socket.on("messageReceived",({firstName,lastName,text})=>{
       console.log(firstName + "  :  " +text);
-      setMessages((messages)=>[...messages,{firstName,text}]);
+      setMessages((messages)=>[...messages,{firstName,lastName,text}]);
     });
 
     return()=>{
@@ -35,13 +57,15 @@ const Chat = () => {
   }
 
   return (
-    <div  className="w-1/2 mx-auto bg-base-200 border border-gray-700 m-5 h-[70vh] flex flex-col shadow-lg">
+    <div className="w-full max-w-3xl mx-auto bg-base-200 border border-gray-700 m-5 h-[80vh] flex flex-col shadow-lg rounded-xl">
+
 
       <h1 className="p-5 border-b border-gray-700">chat</h1>
-      <div className="flex-1 overflow-scroll p-5">
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+
         {messages.map((msg,index)=>{
           return(
-            <div key={index} className="chat chat-start">
+            <div key={index} className={"chat " +(user.firstName===msg.firstName? "chat-end" :"chat-start")}>
               <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
                   <img
@@ -51,8 +75,8 @@ const Chat = () => {
                 </div>
               </div>
               <div className="chat-header">
-                {msg.firstName}
-                <time className="text-xs opacity-50">12:45</time>
+                {`${msg.firstName}  ${msg.lastName}`}
+                <time className="text-xs opacity-50">2 hours ago</time>
               </div>
               <div className="chat-bubble">{msg.text}</div>
               <div className="chat-footer opacity-50">Delivered</div>
@@ -60,7 +84,8 @@ const Chat = () => {
           );
         })}
       </div>
-      <div className="p-5 bg-base-200 border border-gray-700 flex item-center gap-2  shadow-lg">
+      <div className="p-4 border-t border-gray-700 flex items-center gap-2 bg-base-300">
+
         <input 
           value={newMessage}
           onChange={(e)=>setNewMessage(e.target.value)}
