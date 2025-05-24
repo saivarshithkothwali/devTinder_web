4b648167fa592a6582012beee702844732f6ca60
Sai Varshith Kothwali <saivarshithkothwali@gamil.com>
2025-05-24 10:25:37 +0530

    Error in feed

--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -2,12 +2,13 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const feedSlice=createSlice({
   name:"feed",
-  initialState:null,
+  initialState:[],
   reducers:{
     addFeed:(state,action)=>{
       return action.payload;
     },
     removeUserFromFeed:(state,action)=>{
+      if (!state) return state;
       const newFeed=state.filter((user)=>user._id!==action.payload);
       return newFeed;
     },
